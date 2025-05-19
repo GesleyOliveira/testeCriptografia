f0b55213c357d26de70e8ca22d5715d795b6a65c
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2025-05-19 20:06:39 -0300

    style: style from login page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/register';
+
+import './styles/theme.css';
+import './styles/global.css';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
